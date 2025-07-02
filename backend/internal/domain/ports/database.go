d4be3733e4a959afde98b957d3b7e45e7dd56f59
Danila <74253232+Tanax-Xt@users.noreply.github.com>
2025-07-02 13:38:21 +0300

    refactor: refactor db init

--- backend/internal/domain/ports/database.go
+++ backend/internal/domain/ports/database.go
@@ -1,12 +1,13 @@
-package config
+package ports
 
 import (
 	"fmt"
+	"github.com/isiyar/daily-energy/backend/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func InitDatabase(c Config) (*gorm.DB, error) {
+func InitDatabase(c config.Config) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", c.DB_HOST, c.DB_PORT, c.DB_USERNAME, c.DB_PASSWORD, c.DB_NAME)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
