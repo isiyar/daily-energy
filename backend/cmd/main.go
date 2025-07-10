7126331894cff9a370820146788e2b7c07be8353
wannasleep <wannasleeprightnowww@gmail.com>
2025-07-10 20:08:53 +0500

    fix: change tunnel

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -61,7 +61,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://frontend-dev:5173", "https://test-srvr.ru", "https://2ec3ca0382f7.ngrok-free.app"},
+		AllowOrigins:     []string{"http://frontend-dev:5173", "https://test-srvr.ru", "https://342a6b1ace7e.ngrok-free.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "initdata"},
 		ExposeHeaders:    []string{"Content-Length"},
